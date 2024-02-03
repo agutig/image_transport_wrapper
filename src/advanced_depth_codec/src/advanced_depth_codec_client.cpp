f3558e6b9d23362b8b8fccf524228e0ee60878a9
Álvaro Gutiérrez García <alvarogutierrezg99@gmail.com>
2024-02-03 18:01:55 +0100

    added new package

---
+++ src/advanced_depth_codec/src/advanced_depth_codec_client.cpp
@@ -0,0 +1,46 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
+#include "depth_codec/depth_codec.hpp"
+
+const std::string TOPIC_IN = "depth_server_camera_image";
+const std::string TOPIC_OUT = "depth_client_camera_image";
+
+class advanced_depth_codec_client : public rclcpp::Node
+{
+public:
+  advanced_depth_codec_client() : Node("advanced_depth_codec_client")
+  {
+    RCLCPP_INFO(this->get_logger(), "Listening on: %s", TOPIC_IN.c_str());
+    RCLCPP_INFO(this->get_logger(), "Ready to publish on: %s", TOPIC_OUT.c_str());
+
+    // Define the QoS profile
+    auto qos = rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_sensor_data));
+    qos.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
+    qos.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
+    qos.history(RMW_QOS_POLICY_HISTORY_KEEP_LAST);
+    qos.keep_last(1);
+
+    publisher_ = this->create_publisher<sensor_msgs::msg::Image>(TOPIC_OUT, qos);
+    subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+      TOPIC_IN, qos,
+      [this](sensor_msgs::msg::Image::SharedPtr msg) {
+
+        RCLCPP_INFO(this->get_logger(), "Connected to topics, sending video...");
+        auto codec_msg = to_decode_frame(msg);
+
+        publisher_->publish(*codec_msg);
+      });
+  }
+
+private:
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+};
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<advanced_depth_codec_client>());
+  rclcpp::shutdown();
+  return 0;
+}
