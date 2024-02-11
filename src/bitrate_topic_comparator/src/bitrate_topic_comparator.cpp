111cb5150e9084d9c4b13202402af7af31995cba
Álvaro Gutiérrez García <alvarogutierrezg99@gmail.com>
2024-02-11 14:09:02 +0100

    update

--- src/bitrate_topic_comparator/src/bitrate_topic_comparator.cpp
+++ src/bitrate_topic_comparator/src/bitrate_topic_comparator.cpp
@@ -7,61 +7,93 @@
 using namespace std::chrono_literals;
 
 const std::string TOPIC_1 = "depth_camera_image";
+const std::string TOPIC_2 = "depth_server_camera_image"; // Definimos el segundo topic
 
 class BitrateTopicComparator : public rclcpp::Node {
 public:
     BitrateTopicComparator()
-    : Node("bitrate_topic_comparator"), last_bitrate_(-1000.0), last_message_time_(this->now()) {
+    : Node("bitrate_topic_comparator"),
+      last_bitrate_1_(-1000.0), last_message_time_1_(this->now()),
+      last_bitrate_2_(-1000.0), last_message_time_2_(this->now()) { // Inicializamos las variables para ambos topics
         auto qos = rclcpp::QoS(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_sensor_data));
         qos.reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT);
         qos.durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
         qos.history(RMW_QOS_POLICY_HISTORY_KEEP_LAST);
         qos.keep_last(1);
-        subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
-            TOPIC_1, qos, std::bind(&BitrateTopicComparator::topic_callback, this, std::placeholders::_1));
+
+        // Suscripción al primer topic
+        subscription_1_ = this->create_subscription<sensor_msgs::msg::Image>(
+            TOPIC_1, qos, std::bind(&BitrateTopicComparator::topic_callback_1, this, std::placeholders::_1));
+            
+        // Suscripción al segundo topic
+        subscription_2_ = this->create_subscription<sensor_msgs::msg::Image>(
+            TOPIC_2, qos, std::bind(&BitrateTopicComparator::topic_callback_2, this, std::placeholders::_1));
 
         timer_ = this->create_wall_timer(
             1s, std::bind(&BitrateTopicComparator::timer_callback, this));
     }
 
 private:
-    void topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) {
+    // Callback para el primer topic
+    void topic_callback_1(const sensor_msgs::msg::Image::SharedPtr msg) {
+        calculate_bitrate(msg, last_bitrate_1_, last_message_time_1_);
+    }
+    
+    // Callback para el segundo topic
+    void topic_callback_2(const sensor_msgs::msg::Image::SharedPtr msg) {
+        calculate_bitrate(msg, last_bitrate_2_, last_message_time_2_);
+    }
 
-        //RCLCPP_INFO(this->get_logger(), "Mensaje recibido en topic '%s'", TOPIC_1.c_str());
+    // Función para calcular el bitrate
+    void calculate_bitrate(const sensor_msgs::msg::Image::SharedPtr& msg, double& last_bitrate, rclcpp::Time& last_message_time) {
         auto now = this->now();
-        auto time_diff = now - last_message_time_;
+        auto time_diff = now - last_message_time;
         auto time_diff_sec = time_diff.seconds() + time_diff.nanoseconds() / 1e9;
 
         if (time_diff_sec > 0) {
             size_t message_size_bits = calculate_serialized_size(msg);
-            last_bitrate_ = message_size_bits / time_diff_sec;
-            last_bitrate_ = last_bitrate_ / 1e6;
+            last_bitrate = message_size_bits / time_diff_sec / 1e6; // Mbits/sec
         }
 
-        last_message_time_ = now;
+        last_message_time = now;
     }
 
+    // Timer callback para imprimir los bitrates
     void timer_callback() {
-        if (last_bitrate_ <= 0) {
-            RCLCPP_INFO(this->get_logger(), "%s -- NO data", TOPIC_1.c_str());
-        } else {
-            RCLCPP_INFO(this->get_logger(), "%s -- Bitrate: %f Mbits/sec", TOPIC_1.c_str(), last_bitrate_);
-        }
-        last_bitrate_ = 0;
+      std::stringstream ss;
+      // Para el primer topic
+      if (last_bitrate_1_ <= 0) {
+          ss << TOPIC_1 << " Bitrate: No data";
+      } else {
+          ss << TOPIC_1 << " Bitrate: " << last_bitrate_1_ << " Mbits/sec";
+      }
+      // Añade una coma y espacio para separar los mensajes de los topics
+      ss << ", ";
+      // Para el segundo topic
+      if (last_bitrate_2_ <= 0) {
+          ss << TOPIC_2 << " Bitrate: No data";
+      } else {
+          ss << TOPIC_2 << " Bitrate: " << last_bitrate_2_ << " Mbits/sec";
+      }
+
+      RCLCPP_INFO(this->get_logger(), "%s", ss.str().c_str());
+
+      last_bitrate_1_ = -1000;
+      last_bitrate_2_ = -1000;
     }
 
+    // Función para calcular el tamaño serializado del mensaje
     size_t calculate_serialized_size(const sensor_msgs::msg::Image::SharedPtr& msg) {
         rclcpp::Serialization<sensor_msgs::msg::Image> serializer;
         rclcpp::SerializedMessage serialized_msg;
         serializer.serialize_message(msg.get(), &serialized_msg);
-        size_t serialized_size_bytes = serialized_msg.size();
-        return serialized_size_bytes * 8;
+        return serialized_msg.size() * 8; // bits
     }
 
-    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_;
+    rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr subscription_1_, subscription_2_;
     rclcpp::TimerBase::SharedPtr timer_;
-    double last_bitrate_;
-    rclcpp::Time last_message_time_;
+    double last_bitrate_1_, last_bitrate_2_;
+    rclcpp::Time last_message_time_1_, last_message_time_2_;
 };
 
 int main(int argc, char* argv[]) {
@@ -71,4 +103,3 @@ int main(int argc, char* argv[]) {
     rclcpp::shutdown();
     return 0;
 }
-
